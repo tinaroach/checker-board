6502b9459f5ac771ef47ab22fe81319f6b6b2f4e
tinaroach <16093025@massey.ac.nz>
2016-05-12 12:59:03 +1200

    created two lines of pieces

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+to place/spawn a piece using JavaScript:
+
+var piece = document.createElement('div');            // create an empty div
+piece.setAttribute('class', 'piece');                 // assign the div class="piece"
+document.getElementById('board').appendChild(piece);  // add the div to the id="board"
+piece.style.top = '0px';                              // assign the top/y position
+piece.style.left = '0px';                             // assign the left/x position
+*/
